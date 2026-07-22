d5852e93967962753a32b56774532d18481dbf85
bilenray <raybimlendra@gmail.com>
2026-07-22 17:55:24 -0500

    Performed open loop test for steep steer, added readme

--- phase0/scripts/run_open_loop_steep_steer_test.m
+++ phase0/scripts/run_open_loop_steep_steer_test.m
@@ -2,23 +2,27 @@
 
 startup_project;
 
-modelName = "plant_open_loop_test";
+modelName = "plant_open_loop_step_steer_test";
 
 % Test configuration
-testStopTime_s = 100;
+testStopTime_s = 500;
+test_step_time_s = 250; %for step response
 test_braking_cmd = 0;
-test_steering_wheel_deg = 0;
+test_steering_wheel_deg = 40;
 test_slope = 0;
 test_road_friction_mu = 0.95; % [1]
 
 % torque range
-torqueLevels_Nm = [100, 150, 200, 250, 300];
+torqueLevels_Nm = 100;
 
 results = struct([]);
 
 for k = 1:numel(torqueLevels_Nm)
 
     simInput = Simulink.SimulationInput(modelName);
+    
+    simInput = simInput.setVariable( ...
+        "test_step_time_s", test_step_time_s);
 
     simInput = simInput.setVariable( ...
         "test_drive_torque_Nm", torqueLevels_Nm(k));
@@ -37,19 +41,22 @@ for k = 1:numel(torqueLevels_Nm)
 
     simInput = simInput.setModelParameter( ...
         StopTime=string(testStopTime_s));
-
+    
     simOut = sim(simInput);
     
     logs = simOut.logsout;
 
     vxSignal = logs.get("vx_mps").Values;
     axSignal = logs.get("ax_mps2").Values;
+    yawRateSignal = logs.get("yaw_rate_radps").Values;
+    yawSignal = logs.get("yaw_angle_rad").Values;
 
     results(k).torque_Nm = torqueLevels_Nm(k);
     results(k).time_s = vxSignal.Time;
     results(k).vx_mps = vxSignal.Data;
-    results(k).ax_time_s = vxSignal.Time;
     results(k).ax_mps2 = axSignal.Data;
+    results(k).yaw_rate_radps = yawRateSignal.Data;
+    results(k).yaw_angle_rad = yawSignal.Data;
 end
 
 
@@ -74,13 +81,51 @@ ylabel("Longitudinal speed, V_x (m/s)");
 title("Open-loop response to constant drive torque (velocity)");
 legend(labels, Location = "best");
 
+%% yaw rate
+figure;
+hold on;
+labels = strings(1, numel(results));
+for k = 1:numel(results)
+    plot(...
+        results(k).time_s, ...
+        results(k).yaw_rate_radps, ...
+        linestyles{k} ...
+        );
+    labels(k) = sprintf("%d N.m", results(k).torque_Nm);
+end
+
+grid on;
+xlabel("Time (s)");
+ylabel("Yaw rate (rad/s)");
+title("Open-loop response of yaw rate to constant velocity");
+legend(labels, Location = "best");
+
+%% yaw angle
+figure;
+hold on;
+labels = strings(1, numel(results));
+for k = 1:numel(results)
+    plot(...
+        results(k).time_s, ...
+        results(k).yaw_angle_rad, ...
+        linestyles{k} ...
+        );
+    labels(k) = sprintf("%d N.m", results(k).torque_Nm);
+end
+
+grid on;
+xlabel("Time (s)");
+ylabel("Yaw angle (rad)");
+title("Open-loop response of yaw angle at const velocity");
+legend(labels, Location = "best");
+
 %% acceleration
 % figure;
 % hold on;
 % labels = strings(1, numel(results));
 % for k = 1:numel(results)
 %     plot( ...
-%         results(k).ax_time_s, ...
+%         results(k).time_s, ...
 %         results(k).ax_mps2, ...
 %         linestyles{k} ...
 %     );
